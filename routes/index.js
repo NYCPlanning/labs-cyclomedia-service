1526d5bb6a489d04b19f57c5662cb4691eed83fc
allthesignals <wilburnforce@gmail.com>
2017-11-24 16:20:27 -0500

    Better routing

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const proj4 = require('proj4');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', ({ query: { lat, lon } }, res) => {
+router.get('/view/:lon/:lat', ({ params: { lat, lon } }, res) => {
   const fromCRS = '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs';
   const to = '+proj=utm +zone=18 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs';
   const { USERNAME, PASS, APIKEY } = process.env;
@@ -14,7 +14,7 @@ router.get('/', ({ query: { lat, lon } }, res) => {
     parseFloat(lat),
   ];
 
-  const query = proj4(fromCRS, to, coord);
+  const query = (lon && lat) ? proj4(fromCRS, to, coord) : '';
 
   res.render('index', {
     query, USERNAME, PASS, APIKEY,

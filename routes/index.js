0bf1e19b3d3166243f3947082f96a675b574772f
allthesignals <wilburnforce@gmail.com>
2017-11-24 15:41:19 -0500

    ESLint

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,24 @@
-var express = require('express');
-var proj4 = require('proj4');
-var router = express.Router();
+const express = require('express');
+const proj4 = require('proj4');
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function({ query: { lat, lon } }, res, next) {
-  var fromCRS = '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs';
-  var to = '+proj=utm +zone=18 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs';
-  var { USERNAME, PASS, APIKEY } = process.env;
+router.get('/', ({ query: { lat, lon } }, res) => {
+  const fromCRS = '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs';
+  const to = '+proj=utm +zone=18 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs';
+  const { USERNAME, PASS, APIKEY } = process.env;
 
-  var coord = [
+  const coord = [
     parseFloat(lon),
     parseFloat(lat),
   ];
 
-  var query = proj4(fromCRS, to, coord);
+  const query = proj4(fromCRS, to, coord);
 
-  res.render('index', { query, USERNAME, PASS, APIKEY });
+  res.render('index', {
+    query, USERNAME, PASS, APIKEY,
+  });
 });
 
 module.exports = router;
